23ff5b84829fe60609cfe9884faaa4b49d5a354e
Aaron <apurjh@gmail.com>
2017-02-06 19:54:27 +0000

    member model

---
+++ models/member.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const membersSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  emailAddress: { type: String, required: true, unique: true },
+  school: [{ type: mongoose.Schema.ObjectId, ref: 'School' }]
+});
+
+module.exports = mongoose.model('Member', membersSchema);
